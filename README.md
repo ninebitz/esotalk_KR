3446cca7838871fc42af875c2b332fe2ae944f71
Young-sok Ki <ninebitz@gmail.com>
2014-12-17 10:40:35 +0900

    Some changes

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 esoTalk_KR
 ==========
 
-esoTalk Korean Translation.
-based on [Japanese Translation Project](https://github.com/Pigmiral/esoTalk_JP)
+esoTalk Korean Translation
+based on [Japanese Translation Project](https://github.com/Pigmiral/esoTalk_JP).
+
+Installation:
+=================
+Just clone this repo into esoTalkRoot/addons/languages/Korean:
+
+~~~bash
+$> cd esoTalkRoot/addons/languages
+$> git clone https://github.com/ninebitz/esotalk_KR.git Korean
+~~~
---
+++ definitions.Answers.php
@@ -0,0 +1,20 @@
+<?php
+// Copyright 2014 ninebitz
+ET::$languageInfo["Korean"] = array(
+	"locale" => "ko-KR",
+	"name" => "Korean",
+	"description" => "esoTalk Answers Korean language file.",
+	"version" => "1.0.0",
+	"author" => "ninebitz",
+	"authorEmail" => "ninebitz@gmail.com",
+	"authorURL" => "https://github.com/ninebitz",
+	"license" => "GPLv2"
+);
+
+$definitions["Answer"] = "답변";
+$definitions["Answered"] = "답변됨";
+$definitions["Answered by %s"] = "%s에 의해 답변됨";
+$definitions["label.answered"] = "답변됨";
+$definitions["Remove answer"] = "답변 삭제";
+$definitions["See post in context"] = "컨텍스트 메뉴에서 글 보기";
+$definitions["This post answered my question"] = "답변 채택";
